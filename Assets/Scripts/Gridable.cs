7f7db98d21e141eb683da652856666df899a5cf6
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-17 18:08:22 -0600

    Rename EditorGrid -> Gridable

---
+++ Assets/Scripts/Gridable.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class Gridable : MonoBehaviour {
+  public float cell_size = 1f;
+
+  void Update()
+  {
+    SnapToGrid();
+  }
+
+  public Vector3 GridPosition()
+  {
+    return new Vector3(GridX(), GridY(), GridZ());
+  }
+
+  public float GridX()
+  {
+    return Mathf.Round(transform.position.x / cell_size) * cell_size;
+  }
+
+  public float GridY()
+  {
+    return Mathf.Round(transform.position.y / cell_size) * cell_size;
+  }
+
+  public float GridZ()
+  {
+    return transform.position.z;
+  }
+
+  public void SnapToGrid()
+  {
+    transform.position = GridPosition();
+  }
+}
